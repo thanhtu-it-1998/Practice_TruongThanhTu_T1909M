35067eb61cb47ae65229c9d467cbb4568bed2c45
THANH TU TRUONG <truongthanhtu.it.1998@gmail.com>
2021-01-21 09:36:09 +0700

    udpate

---
+++ ManageEmployees/ManageEmployees/Migrations/20210121023426_DBv4.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ManageEmployees.Migrations
+{
+    public partial class DBv4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
